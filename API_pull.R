cc610f4b8f0813d468447bc61bdbb447627d5266
MGreels <mgreeley@gmail.com>
2023-05-19 15:28:37 -0400

    Somerville Rain Data, pulls hourly rain data for the Somerville Site

--- API_pull.R
+++ API_pull.R
@@ -1,6 +1,15 @@
 library(jsonlite)
 library(httr)
+library(dplyr)
+library(tidyverse)
 
-res=GET('https://api.meteomatics.com/yesterdayT00:00:00.000-04:00--tomorrow+2DT00:00:00.000-04:00:PT1H/precip_1h:mm/42.392794587,-71.084942664/json',
+res <- GET('https://api.meteomatics.com/yesterdayT00:00:00.000-04:00--yesterdayT23:59:59.000-04:00:PT1H/precip_1h:mm/42.392794587,-71.084942664/json',
         authenticate('mwra_greeley','kk4U3R3cJm'))
+data <- fromJSON(rawToChar(res$content))
+
+Yest_data <- data$data$coordinates[[1]][[3]][[1]] %>%
+    mutate(lat = data$data$coordinates[[1]][[1]],
+           lon = data$data$coordinates[[1]][[2]]
+           )
+
 
