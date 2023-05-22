b2413971790bb71a5aaaea5dc37ab80474479bc8
MGreels <mgreeley@gmail.com>
2023-05-22 11:12:44 -0400

    Added Comments/descriptiosn to final

--- API_pull.R
+++ API_pull.R
@@ -1,6 +1,6 @@
 #############
 #   Pulls API data for single points contained in 'rain_sites.xlsx
-#   
+#   A daily run will include the historical data for 
 
 
 library(jsonlite)
@@ -11,15 +11,22 @@ library(readr)
 library(readxl)
 
 x <-read_xlsx('rain_sites.xlsx', col_types = 'text' )
+
+##creates the build string funciton
 source('build_api_string_func.R')
+##R
 if(exists('Yest_data')){
     rm(Yest_data)
 }
 
+## runs a for loop, for every site, long,lat combo in "rain_sites.xlsx" 
+## creates an API link and pull the data from the meteopmatics API.
+## Pulls hourly rainfall totals from each site from yesterday.
 for (i in 1:nrow(x)) {
 res <- GET(build_APIstring(x$Lat[i], x$Lon[i]),
         authenticate('mwra_greeley','kk4U3R3cJm'))
 res <- fromJSON(rawToChar(res$content))
+
 
 Yest_data_site <- res$data$coordinates[[1]][[3]][[1]] %>%
     mutate(lat = res$data$coordinates[[1]][[1]],
@@ -32,18 +39,24 @@ Yest_data <-
     
 }
 rm(i)
-##pulls old data from n
+## converts the character date data to POSIXct then converts from UTC to EST
 Yest_data$date <- Yest_data$date %>%
     as.POSIXct(format="%Y-%m-%dT%H:%M:%OSZ", tz = 'UTC') %>%
     format(tz = 'EST') %>%
     as.POSIXct()
 
+## Pulls in current CSV file and compares old data to new data
+## This step throws out any repeat time-site combos
+## script will replaces with the new data if there is a conflict.
 Old_data <- read_csv('rainfall_data.csv')
-Old_data <- anti_join(Old_data, Yest_data, by = c("date", "site"))##removes old data
+Old_data <- anti_join(Old_data, Yest_data, by = c("date", "site"))
 
 ##bind API pull from Yesterday into old data table
 New_data <- rbind(Old_data,Yest_data)
+
+## writes new csv file including yesterday's data.
 write_csv(New_data,"rainfall_data.csv")
 
-rm(res,x,Yest_data_site,Old_data,Yest_data)
+rm(res,x,Yest_data_site,Old_data,Yest_data, build_APIstring)
+rm(New_data)
 
--- build_api_string_func.R
+++ build_api_string_func.R
@@ -1,4 +1,12 @@
+## Builds a function to accept longitude and latitude strings and create a
+## point API link from meteomatics for hourly rainfall data at a specific point
+## See Link Below for info on API
+## https://www.meteomatics.com/en/api/getting-started/?ppc_keyword=meteomatics&gclid=CjwKCAjwpayjBhAnEiwA-7enawv9LQVyDluwDl8jLIftL_aN-0gLfF7v5LZJ-eA9HOPJBrozzwyU7RoCprQQAvD_BwE
+
+
 build_APIstring <- function(lat, lon) {
+
+    
 
 api1 <-'https://api.meteomatics.com/yesterdayT00:00:00.000-05:00--yesterdayT23:59:59.000-05:00:PT1H/precip_1h:mm/'
     
@@ -6,7 +14,9 @@ api2 <-    ','
     
 api3 <-    '/json'
 
-paste(api1,lat,api2, lon, api3, sep = '')
+## pastes 5 strings together to add the latitude and longitude portions 
+## to the meteomatics API call
+paste(api1,lat,api2, lon, api3, sep = '') 
         
 
 }
